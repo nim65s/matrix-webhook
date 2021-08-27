4928ceb91a8b6d0a70338a6fa583a8fbdb173c31
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-08-27 23:59:49 +0200

    release: details

--- docs/release.sh
+++ docs/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 # ./docs/release.sh [patch|minor|major|x.y.z]
 
-[[ $(basename $PWD) == docs ]] && cd ..
+[[ $(basename "$PWD") == docs ]] && cd ..
 
 
 OLD=$(poetry version -s)
 
-poetry version $1
+poetry version "$1"
 
 NEW=$(poetry version -s)
 DATE=$(date +%Y-%m-%d)
@@ -15,6 +15,6 @@ sed -i "/^## \[Unreleased\]/a \\\n## [$NEW] - $DATE" CHANGELOG.md
 sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
 sed -i "/^\[Unreleased\]/a [$NEW] https://github.com/nim65s/matrix-webhook/compare/v$OLD...v$NEW" CHANGELOG.md
 
-echo git add pyproject.toml CHANGELOG.md
-echo git commit -m "Release v$NEW"
-echo git tag -s "v$NEW" -m "Release v$NEW"
+git add pyproject.toml CHANGELOG.md
+git commit -m "Release v$NEW"
+git tag -s "v$NEW" -m "Release v$NEW"
