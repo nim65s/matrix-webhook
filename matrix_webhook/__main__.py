29653fc04b821af50f3b95701ef9ccc344fde303
Albrecht Muehlenschulte <muehlenschulte.albrecht@fh-swf.de>
2022-05-06 12:43:33 +0200

    adds script

--- matrix_webhook/__main__.py
+++ matrix_webhook/__main__.py
@@ -3,7 +3,12 @@ import logging
 
 from . import app, conf
 
-if __name__ == "__main__":
+
+def main():
     log_format = "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s"
     logging.basicConfig(level=50 - 10 * conf.VERBOSE, format=log_format)
     app.run()
+
+
+if __name__ == "__main__":
+    main()
