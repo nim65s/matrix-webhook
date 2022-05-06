843209f0891bf59489b32489225a8f9e820e71ad
Albrecht Muehlenschulte <muehlenschulte.albrecht@fh-swf.de>
2022-05-06 12:43:33 +0200

    adds script

--- matrix_webhook/run.py
+++ matrix_webhook/run.py
@@ -3,7 +3,9 @@ import logging
 
 from . import app, conf
 
-if __name__ == "__main__":
+
+def run():
     log_format = "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s"
     logging.basicConfig(level=50 - 10 * conf.VERBOSE, format=log_format)
     app.run()
+
