c04dec629b6a331d36567aea07ed160af4d027c8
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-03-08 17:49:17 +0100

    fix pydocstyle

--- tests/start.py
+++ tests/start.py
@@ -51,9 +51,7 @@ def wait_available(url: str, key: str, timeout: int = 10) -> bool:
     """Wait until a service answer correctly or timeout."""
 
     def check_json(url: str, key: str) -> bool:
-        """
-        Ensure a service at a given url answers with valid json including a certain key.
-        """
+        """Ensure a service answers with valid json including a certain key."""
         try:
             data = httpx.get(url).json()
             return key in data
