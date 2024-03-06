9deea93d43b31d6c3d4e5b2aaca53f2733f0aaeb
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-06 17:51:53 +0100

    try reconnections on LocalProtocolError, fix #132

--- matrix_webhook/utils.py
+++ matrix_webhook/utils.py
@@ -59,6 +59,9 @@ async def join_room(room_id):
         except LocalProtocolError as e:
             msg = f"Send error: {e}"
             LOGGER.error(msg)
+            LOGGER.warning("Reconnecting")
+            if conf.MATRIX_PW:
+                await CLIENT.login(conf.MATRIX_PW)
         LOGGER.warning("Trying again")
     return create_json_response(HTTPStatus.GATEWAY_TIMEOUT, "Homeserver not responding")
 
