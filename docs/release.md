11564bb642486af7622bd5205e53bfa2a65e4fb5
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-09 00:30:28 +0100

    fix release

--- docs/release.md
+++ docs/release.md
@@ -5,10 +5,11 @@ To trigger it, we just need to:
 
 1. use poetry to update the version number
 2. update the changelog
-3. git commit
-4. git tag
-5. git push
-6. git push --tags
+3. update version number in flake.nix
+4. git commit
+5. git tag
+6. git push
+7. git push --tags
 
 
 For this, an helper script is provided:
--- docs/release.sh
+++ docs/release.sh
@@ -14,7 +14,7 @@ DATE=$(date +%Y-%m-%d)
 sed -i "/^## \[Unreleased\]/a \\\n## [v$NEW] - $DATE" CHANGELOG.md
 sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
 sed -i "/^\[Unreleased\]/a [v$NEW]: https://github.com/nim65s/matrix-webhook/compare/v$OLD...v$NEW" CHANGELOG.md
-sed -i "/version/$OLD/$NEW/" flake.nix
+sed -i "/version/s/$OLD/$NEW/" flake.nix
 
 git add pyproject.toml CHANGELOG.md
 git commit -m "Release v$NEW"
